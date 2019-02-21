975ee7d896ccf5af97a0713a2242f9f12bcd8fce
TaroSunagawa <taroman73@gmail.com>
2019-02-21 11:43:37 +0900

    add one neuron IF model

--- leaky_integrate_and_fire.py
+++ leaky_integrate_and_fire.py
@@ -5,14 +5,14 @@ import matplotlib.pyplot as plt
 dt = 0.01#ms
 C = 1.0#uF/cm^2
 E_L = 10.6#mV
-
 t_plot = []
 pre_v_plot = []
 post_v_plot = []
 #---------------------------------------------------------------------------------------------------------------
 
 def calc_v(P_s, v):
-    dvdt = (1.0/tau_m)*(E_L - v - r_mg_s*P_s*(v-E_s) + R_mI_e )
+    #dvdt = (1.0/tau_m)*(E_L - v - r_mg_s*P_s*(v-E_s) + R_mI_e )
+    dvdt = (1.0/tau_m)*(E_L - v - r_mg_s*(v-E_s) + R_mI_e )
     v = v + dvdt*dt
     return v
 
@@ -22,15 +22,14 @@ def P_syn(ft, t):
 if __name__ == '__main__':
     #initialization
     E_L = -70#mV
-    E_s = 0.0#mV(excitatory)   -80.0mV(inhibitory)
+    E_s = 0.0#mV
     v_th = -54#mV
     v_reset = -80#mV
     tau_m = 20#ms
     tau_s = 10#ms
     P_max = 1.0
     r_mg_s = 0.05
-    R_mI_e = 25
-    
+    R_mI_e = 20
     pre_v = -65.0 #mV
     post_v = -70.0#mV
     P_s_pre = 0.0
@@ -46,22 +45,20 @@ if __name__ == '__main__':
         R_mI_e = R_mI_e
         
         pre_v = calc_v(P_s_pre, pre_v)
-        #print('pre:', pre_v)
         pre_v_plot = np.append(pre_v_plot, pre_v)
         
-        post_v = calc_v(P_s_post, post_v)
-        #print('post:', post_v)
-        post_v_plot = np.append(post_v_plot, post_v)
+        #post_v = calc_v(P_s_post, post_v)
+        #post_v_plot = np.append(post_v_plot, post_v)
         
         if pre_v > v_th:
-            P_s_post = 1.0
-            pspost_plot = np.append(pspost_plot, P_s_post)
+            #P_s_post = 1.0
+            #pspost_plot = np.append(pspost_plot, P_s_post)
             pre_v = v_reset
             ft_pre = t
-        else:
-            P_s_post = P_syn(ft_pre, t)
-            pspost_plot = np.append(pspost_plot, P_s_post)
-       
+        #else:
+            #P_s_post = P_syn(ft_pre, t)
+            #pspost_plot = np.append(pspost_plot, P_s_post)
+        """
         if post_v > v_th:
             P_s_pre = 1.0
             pspre_plot = np.append(pspre_plot, P_s_pre)
@@ -70,12 +67,10 @@ if __name__ == '__main__':
         else:
             P_s_pre = P_syn(ft_post, t)
             pspre_plot = np.append(pspre_plot, P_s_pre)
-        
+        """
         t_plot = np.append(t_plot, t)
         t += dt
   
-    #print(pre_v_plot)
-    #print(post_v_plot)
     fig = plt.figure(figsize=(10,5),dpi=200)
     plt.xlabel('t (ms)')
     plt.ylabel('V (mV)')
@@ -84,8 +79,8 @@ if __name__ == '__main__':
     #ax0.set_ylim([0,6])
     #plt.plot(t_plot, pspre_plot, label='pre')
     #plt.plot(t_plot, pspost_plot, label='post')
-    plt.plot(t_plot, pre_v_plot, label='pre')#, color='blue')
-    plt.plot(t_plot, post_v_plot, label='post')#, color='orage')
+    plt.plot(t_plot, pre_v_plot)#, color='blue')
+    #plt.plot(t_plot, post_v_plot, label='post')#, color='orage')
     plt.legend()
     #plt.savefig('fig.png')
     plt.show()
